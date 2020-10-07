a4ae7ae2a518e1c0716142897d4a3568df4144eb
estatereal747 <estatereal747@gmail.com>
2020-10-08 00:58:10 +0530

    added day6

---
+++ Amalu/day6/demo-1/src/test/java/com/example/Assign6/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assign6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
