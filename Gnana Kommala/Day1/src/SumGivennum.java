18798365ea1f2de71046c3336f7917a89392bc1c
USTGLOBLE-2020 <72395955+USTGLOBLE-2020@users.noreply.github.com>
2020-10-07 14:28:38 +0530

    Add files via upload

---
+++ Gnana Kommala/Day1/src/SumGivennum.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class SumGivennum {
+
+	static int getSum(int n) {
+		int sum = 0;
+
+		while (n != 0) {
+			sum = sum + n % 10;
+			n = n / 10;
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int n = 324;
+		System.out.println(getSum(n));
+	}
+}
